d15de0fb9679740d7b120ce4bd9d11d6bc486b31
Nirvedh Meshram <nirvedh@nod-labs.com>
2022-02-18 19:13:06 -0500

    Nod.ai fine tuning parameters for M1

--- iree/compiler/Codegen/SPIRV/AdrenoConfig.cpp
+++ iree/compiler/Codegen/SPIRV/AdrenoConfig.cpp
@@ -30,13 +30,13 @@ LogicalResult setAdrenoCodeGenConfig(const spirv::TargetEnv &targetEnv,
   int64_t subgroupSize = targetEnv.getResourceLimits().subgroup_size().getInt();
   return TypeSwitch<Operation *, LogicalResult>(rootOp)
       .Case<linalg::BatchMatmulOp, linalg::MatmulOp>([](auto op) {
-        std::array<int64_t, 2> workgroupXY = {32, 2};
-        std::array<int64_t, 3> threadMNK = {16, 4, 4};
+        std::array<int64_t, 2> workgroupXY = {256, 1};
+        std::array<int64_t, 3> threadMNK = {4, 4, 4};
         return setMatmulOpConfig(op, workgroupXY, threadMNK);
       })
       .Case<linalg::Conv2DNhwcHwcfOp>([subgroupSize](auto op) {
         return setConvOpConfig(op, subgroupSize,
-                               /*bestTilingFactor=*/32);
+                               /*bestTilingFactor=*/16);
       })
       .Case<linalg::DepthwiseConv2DNhwcHwcOp>([subgroupSize](auto op) {
         return setConvOpConfig(op, subgroupSize,
