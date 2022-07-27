13dd8fba6e6adda1d2677e7d210a185c473d153f
Stanley Winata <stanley@nod-labs.com>
2022-07-27 00:21:28 -0700

    Add more capabilities and extension for LevelZero

--- compiler/src/iree/compiler/Codegen/SPIRV/ConvertToSPIRVPass.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/ConvertToSPIRVPass.cpp
@@ -594,7 +594,7 @@ void ConvertToSPIRVPass::runOnOperation() {
   spirv::AddressingModel addressingModel = spirv::AddressingModel::Logical;
   spirv::MemoryModel memoryModel = spirv::MemoryModel::GLSL450;
   if (hasKernelCapabilty) {
-    addressingModel = spirv::AddressingModel::Physical32;
+    addressingModel = spirv::AddressingModel::Physical64;
     memoryModel = spirv::MemoryModel::OpenCL;
   }
   auto builder = OpBuilder::atBlockBegin(moduleOp.getBody());
