b88e447c1daa555d49f5c07a7d037b7485bbe627
Ranvirsv <ranvirsv@gmail.com>
2023-05-26 10:17:30 -0700

    Fixing fmaxf for CPU 16 bit

---
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/ExpandF16MaxFToF32Pass.cpp
@@ -0,0 +1,74 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Codegen/LLVMCPU/LLVMCPUPasses.h"
+#include "iree/compiler/Codegen/PassDetail.h"
+#include "mlir/Dialect/Vector/Transforms/LoweringPatterns.h"
+#include "mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+
+namespace mlir {
+namespace iree_compiler {
+
+namespace {
+
+struct ExpandF16MaxFToF32Pattern : public OpRewritePattern<arith::MaxFOp> {
+  public:
+    using OpRewritePattern<arith::MaxFOp>::OpRewritePattern;
+
+    LogicalResult matchAndRewrite(arith::MaxFOp op,
+                                  PatternRewriter &rewriter) const override{
+      Type resultType = op.getLhs().getType();
+      if (getElementTypeOrSelf(resultType).getIntOrFloatBitWidth() != 16) {
+        return failure();
+      }
+
+      Location loc = op.getLoc();
+
+      Type wideType = rewriter.getF32Type();
+      if(auto vecTy = resultType.dyn_cast<VectorType>()) {
+        wideType = VectorType::get(vecTy.getShape(), wideType);
+      }
+
+      Value lhsExt = rewriter.create<arith::ExtFOp>(loc, wideType, op.getLhs());
+      Value rhsExt = rewriter.create<arith::ExtFOp>(loc, wideType, op.getRhs());
+      Value maxExt = 
+          rewriter.create<arith::MaxFOp>(loc, wideType, lhsExt, rhsExt);
+      Value result = rewriter.create<arith::TruncFOp>(loc, resultType, maxExt);
+
+      rewriter.replaceOp(op, result);
+      return success();
+    }
+};
+
+struct ExpandF16MaxFToF32Pass
+    : public ExpandF16MaxFToF32Base<
+          ExpandF16MaxFToF32Pass> {
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<linalg::LinalgDialect>();
+  }
+
+  void runOnOperation() override {
+    MLIRContext *context = &getContext();
+    RewritePatternSet patterns(&getContext());
+    patterns.insert<ExpandF16MaxFToF32Pattern>(
+        context);
+    if (failed(applyPatternsAndFoldGreedily(getOperation(),
+                                            std::move(patterns)))) {
+      return signalPassFailure();
+    }
+  }
+};
+}  // namespace
+
+std::unique_ptr<Pass> createExpandF16MaxFToF32Pass() {
+  return std::make_unique<ExpandF16MaxFToF32Pass>();
+}
+
+}  // namespace iree_compiler
+}  // namespace mlir
