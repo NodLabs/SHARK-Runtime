d60066cfc2e9631f9f6b44cdbae12181397aab28
Max Dawkins <max.dawkins@gmail.com>
2023-09-13 13:44:02 -0400

    [LLVMCPU] Add tiling config pass for special non-root op tiling cases This commit adds a new tiling configuration pass in LLVMCPU. This pass sets a special tiling configuration for reassociated quantized matmuls, since the non-root op of these dispatches require specific tiling to target certain x86 instructions. This pass is a place to set abnormal tile sizes on non-root ops for specific types of workloads.

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.h
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.h
@@ -102,6 +102,9 @@ createLLVMCPUFoldVectorContractUnitDimsPass();
 
 std::unique_ptr<Pass> createLLVMCPUFoldMemRefAliasOpsPass();
 
+std::unique_ptr<OperationPass<func::FuncOp>>
+createSetSpecialTilingConfigsPass();
+
 //------------------------------------------------------------------------------
 // LLVMCPU Codegen specific patterns.
 //------------------------------------------------------------------------------
